8a5055e96102354365791006a7fce0ff24a5b6ba
bf_tsai <tsai19911026@gmail.com>
2023-07-29 00:45:14 +0800

    add bootstrap & google font and icon

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// css
 import './assets/scss/all.scss';
+
+
+// jQuery
+import './assets/js/index';
+import './assets/js/product';
+import './assets/js/shop';
+import './assets/js/blog';
+import './assets/js/faq';
+
+// bootstrap 5
+
 
 console.log("Hello world!");
