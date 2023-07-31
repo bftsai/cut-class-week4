3aceb0036199fce042d05be92187c3caa7f947d7
bf_tsai <tsai19911026@gmail.com>
2023-07-31 11:14:41 +0800

    test jQuery

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 // css
 import './assets/scss/all.scss';
 
-
-// jQuery
-// import './assets/js/index';
-// import './assets/js/product';
-// import './assets/js/shop';
-// import './assets/js/blog';
-// import './assets/js/faq';
-
 // bootstrap 5
 
 
