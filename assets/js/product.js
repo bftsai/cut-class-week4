fa9e76ce0d4984cce1baf4d3cd2fa255f0da249f
bf_tsai <tsai19911026@gmail.com>
2023-08-03 03:11:44 +0800

    adjust type

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
     $('.optical-a').click(function (e) { 
         e.preventDefault();
-        
+        $('.sunglasses').fadeOut(500);
+        $('.function').fadeOut(500);
+        $('.optical').fadeIn(1000);
     });
     $('.sunglasses-a').click(function (e) { 
         e.preventDefault();
-        $('.optical-a').fadeOut(500);
-        $('.function-a').fadeOut(500);
+        $('.optical').fadeOut(500);
+        $('.function').fadeOut(500);
         $('.sunglasses').fadeIn(1000);
     });
     $('.function-a').click(function (e) { 
         e.preventDefault();
-        
+        $('.optical').fadeOut(500);
+        $('.sunglasses').fadeOut(500);
+        $('.function').fadeIn(1000);
     });
 });
