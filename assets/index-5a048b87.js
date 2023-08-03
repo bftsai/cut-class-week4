d4038f212f93474fea9414331e0dd6f654d09562
bf_tsai <tsai19911026@gmail.com>
2023-08-04 01:27:15 +0800

    Updates

---
+++ assets/index-5a048b87.js
@@ -0,0 +1,1 @@
+import"./faq-7cedeb15.js";$(document).ready(function(){$(".privacy-a").click(function(e){e.preventDefault(),$(".index").fadeOut(500),$(".service").fadeOut(500),$(".privacy").fadeIn(1e3)}),$(".service-a").click(function(e){e.preventDefault(),$(".index").fadeOut(500),$(".privacy").fadeOut(500),$(".service").fadeIn(1e3)})});
